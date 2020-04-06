6761498949b4bed4dd6d730c605db42f9f11d644
Monirul Hasan <kmhasan@seu.edu.bd>
2020-04-06 11:13:22 +0600

    OC3

--- LocalSearch/src/main/java/bd/edu/seu/localsearch/LocalSearch.java
+++ LocalSearch/src/main/java/bd/edu/seu/localsearch/LocalSearch.java
@@ -1,19 +1,23 @@
 package bd.edu.seu.localsearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class LocalSearch {
+    private final static boolean DEBUG = false;
+
     // steepest ascent hill climbing
-    public void steepestAscentHillClimbing(int n) {
+    public int[] steepestAscentHillClimbing(int n) {
+        // TODO note to self: FIX THIS!!!
         Random random = new Random();
 
         int board[] = new int[n];
         for (int c = 0; c < board.length; c++)
             board[c] = random.nextInt(n);
         int conflicts = countConflicts(board);
-        System.out.println(Arrays.toString(board) + ": " + conflicts);
-
+//        System.out.println(Arrays.toString(board) + ": " + conflicts);
+//
 
         int bestBoard[] = new int[n];
         int minimumConflicts = conflicts;
@@ -22,8 +26,8 @@ public class LocalSearch {
 
         int previousConflicts = conflicts;
         for (int iteration = 0; iteration < 10; iteration++) {
-            System.out.println("-----------");
-            System.out.println("Iteration #" + iteration);
+//            System.out.println("-----------");
+//            System.out.println("Iteration #" + iteration);
             for (int c = 0; c < board.length; c++) {
                 int currentRow = board[c];
                 for (int r = 0; r < board.length; r++) {
@@ -35,7 +39,7 @@ public class LocalSearch {
                             // we found a better board
                             minimumConflicts = newConflicts;
                             System.arraycopy(board, 0, bestBoard, 0, n);
-                            System.out.println(Arrays.toString(board) + ": " + minimumConflicts);
+//                            System.out.println(Arrays.toString(board) + ": " + minimumConflicts);
                         }
                     }
                 }
@@ -48,63 +52,100 @@ public class LocalSearch {
 
             previousConflicts = minimumConflicts;
             // printing out the current board
-            System.out.println("Best board");
-            System.out.println(Arrays.toString(bestBoard) + ": " + minimumConflicts);
-            System.arraycopy(bestBoard, 0, board, 0, n);
+//            System.out.println("Best board");
+//            System.out.println(Arrays.toString(bestBoard) + ": " + minimumConflicts);
+//            System.arraycopy(bestBoard, 0, board, 0, n);
         }
 
+        return bestBoard;
     }
 
-    public void randomRestartHillClimbing(int n) {
+    private int[] generateRandomBoard(int n) {
         Random random = new Random();
 
         int board[] = new int[n];
         for (int c = 0; c < board.length; c++)
             board[c] = random.nextInt(n);
-        int conflicts = countConflicts(board);
-        System.out.println(Arrays.toString(board) + ": " + conflicts);
 
+        return board;
+    }
 
-        int bestBoard[] = new int[n];
-        int minimumConflicts = conflicts;
+    private int[] getBestNextBoard(int board[]) {
+        int currentConflicts = countConflicts(board);
 
-        System.arraycopy(board, 0, bestBoard, 0, n);
+        int minimumConflicts = currentConflicts;
+        int bestBoard[] = new int[board.length];
+        System.arraycopy(board, 0, bestBoard, 0, board.length);
 
-        int previousConflicts = conflicts;
-        for (int iteration = 0; iteration < 10; iteration++) {
-            System.out.println("-----------");
-            System.out.println("Iteration #" + iteration);
-            for (int c = 0; c < board.length; c++) {
-                int currentRow = board[c];
-                for (int r = 0; r < board.length; r++) {
-                    if (currentRow != r) {
-                        board[c] = r;
-                        int newConflicts = countConflicts(board);
+        for (int c = 0; c < board.length; c++) {
+            int currentRow = board[c];
+            for (int r = 0; r < board.length; r++) {
+                if (currentRow != r) {
+                    board[c] = r;
+                    int newConflicts = countConflicts(board);
 
-                        if (newConflicts < minimumConflicts) {
-                            // we found a better board
-                            minimumConflicts = newConflicts;
-                            System.arraycopy(board, 0, bestBoard, 0, n);
-                            System.out.println(Arrays.toString(board) + ": " + minimumConflicts);
-                        }
+                    if (newConflicts < minimumConflicts) {
+                        // we found a better board
+                        minimumConflicts = newConflicts;
+                        System.arraycopy(board, 0, bestBoard, 0, board.length);
                     }
                 }
-                board[c] = currentRow;
+            }
+            board[c] = currentRow;
+        }
+
+        if (DEBUG) {
+            System.out.println("NEXT    " + Arrays.toString(bestBoard) + ": " + countConflicts(bestBoard));
+        }
+
+        return bestBoard;
+    }
+
+    public int[] randomRestartHillClimbing(int n) {
+        int board[] = generateRandomBoard(n);
+        int conflicts = countConflicts(board);
+
+        int bestBoard[] = new int[n];
+        System.arraycopy(board, 0, bestBoard, 0, n);
+        int minimumConflicts = conflicts;
+
+        for (int iteration = 0; iteration < 50; iteration++) {
+            if (DEBUG) {
+                System.out.println("-----------");
+                System.out.println("Iteration #" + iteration);
+                System.out.println("CURRENT " + Arrays.toString(board) + ": " + countConflicts(board));
             }
 
-            if (minimumConflicts == previousConflicts) {
-                for (int c = 0; c < board.length; c++)
-                    board[c] = random.nextInt(n);
-                // TODO fix this code -- maybe some issue with remembering the best conflict values
+            int currentConflicts = countConflicts(board);
+            if (currentConflicts == 0)
+                break;
+
+            board = getBestNextBoard(board);
+            int nextBoardConflicts = countConflicts(board);
+
+            // TODO try out sideways moves (section 4.1 page 123 of AIMA 3e)
+            if (nextBoardConflicts >= currentConflicts) {
+                // random restart because we couldn't do any better
+                board = generateRandomBoard(n);
+                currentConflicts = countConflicts(board);
+                if (DEBUG) {
+                    System.out.println("RANDOM  " + Arrays.toString(board) + ": " + currentConflicts);
+                }
             } else {
-                previousConflicts = minimumConflicts;
-                // printing out the current board
-                System.out.println("Best board");
-                System.out.println(Arrays.toString(bestBoard) + ": " + minimumConflicts);
-                System.arraycopy(bestBoard, 0, board, 0, n);
+                if (nextBoardConflicts < minimumConflicts) {
+                    minimumConflicts = nextBoardConflicts;
+                    System.arraycopy(board, 0, bestBoard, 0, n);
+                    if (DEBUG) {
+                        System.out.println("UPDATED " + Arrays.toString(bestBoard) + ": " + countConflicts(bestBoard));
+                    }
+                }
             }
         }
 
+        if (DEBUG) {
+            System.out.println("BEST    " + Arrays.toString(bestBoard) + ": " + countConflicts(bestBoard));
+        }
+        return bestBoard;
     }
 
     public int countConflicts(int[] board) {
--- LocalSearch/src/main/java/bd/edu/seu/localsearch/Main.java
+++ LocalSearch/src/main/java/bd/edu/seu/localsearch/Main.java
@@ -1,10 +1,33 @@
 package bd.edu.seu.localsearch;
 
+import java.util.Arrays;
+
 public class Main {
     public Main() {
         LocalSearch localSearch = new LocalSearch();
 //        localSearch.steepestAscentHillClimbing(8);
-        localSearch.randomRestartHillClimbing(8);
+        int n = 8;
+        int success = 0;
+        int totalTrials = 100;
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < totalTrials; i++) {
+            int board[] = localSearch.randomRestartHillClimbing(n);
+            int conflicts = localSearch.countConflicts(board);
+            if (conflicts == 0)
+                success++;
+        }
+        long stop = System.currentTimeMillis();
+        System.out.printf("Time taken: %.2f seconds\n", (stop - start) / 1000.0);
+
+        System.out.printf("%d-Queen Success %d/%d (%.2f%%)\n",
+                n,
+                success,
+                totalTrials,
+                100.0 * success / totalTrials);
+
+//        System.out.println("Solution: " + Arrays.toString(board));
+//        System.out.println("Conflicts: " + conflicts);
     }
 
     public static void main(String args[]) {
